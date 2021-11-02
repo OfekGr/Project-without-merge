44616a4c1472d50e6a5a33a6465869d2550cd4b6
OfekGr <91669052+OfekGr@users.noreply.github.com>
2021-11-02 13:00:59 +0200

    Add files via upload

--- game_cards/Card.py
+++ game_cards/Card.py
@@ -27,111 +27,111 @@ class Card:
 
 #Converting card values into strings (card name)
     def check_sign(self):
-        if self.value <=1:
+        if self.value < 2 or self.value > 14:
             raise AssertionError("Value must be between 2 and 14")
+        if self.suit < 0 or self.suit > 3:
+            raise AssertionError("Suit must be between 0 and 3")
         if self.value == 2 and self.suit == 0:
-            return  "Two of Diamonds"
+            return "Two of Diamonds"
         elif self.value == 2 and self.suit == 1:
-            return  "Two of Spades"
+            return "Two of Spades"
         elif self.value == 2 and self.suit == 2:
             return "Two of Hearts"
         elif self.value == 2 and self.suit == 3:
             return "Two of Clubs"
         if self.value == 3 and self.suit == 0:
-            return  "Three of Diamonds"
+            return "Three of Diamonds"
         elif self.value == 3 and self.suit == 1:
-            return  "Three of Spades"
+            return "Three of Spades"
         elif self.value == 3 and self.suit == 2:
             return "Three of Hearts"
         elif self.value == 3 and self.suit == 3:
             return "Three of Clubs"
         if self.value == 4 and self.suit == 0:
-            return  "Four of Diamonds"
+            return "Four of Diamonds"
         elif self.value == 4 and self.suit == 1:
-            return  "Four of Spades"
+            return "Four of Spades"
         elif self.value == 4 and self.suit == 2:
             return "Four of Hearts"
         elif self.value == 4 and self.suit == 3:
             return "Four of Clubs"
         if self.value == 5 and self.suit == 0:
-            return  "Five of Diamonds"
+            return "Five of Diamonds"
         elif self.value == 5 and self.suit == 1:
-            return  "Five of Spades"
+            return "Five of Spades"
         elif self.value == 5 and self.suit == 2:
             return "Five of Hearts"
         elif self.value == 5 and self.suit == 3:
             return "Five of Clubs"
         if self.value == 6 and self.suit == 0:
-            return  "Six of Diamonds"
+            return "Six of Diamonds"
         elif self.value == 6 and self.suit == 1:
-            return  "Six of Spades"
+            return "Six of Spades"
         elif self.value == 6 and self.suit == 2:
             return "Six of Hearts"
         elif self.value == 6 and self.suit == 3:
             return "Six of Clubs"
         if self.value == 7 and self.suit == 0:
-            return  "Seven of Diamonds"
+            return "Seven of Diamonds"
         elif self.value == 7 and self.suit == 1:
-            return  "Seven of Spades"
+            return "Seven of Spades"
         elif self.value == 7 and self.suit == 2:
             return "Seven of Hearts"
         elif self.value == 7 and self.suit == 3:
             return "Seven of Clubs"
         if self.value == 8 and self.suit == 0:
-            return  "Eight of Diamonds"
+            return "Eight of Diamonds"
         elif self.value == 8 and self.suit == 1:
-            return  "Eight of Spades"
+            return "Eight of Spades"
         elif self.value == 8 and self.suit == 2:
             return "Eight of Hearts"
         elif self.value == 8 and self.suit == 3:
             return "Eight of Clubs"
         if self.value == 9 and self.suit == 0:
-            return  "Nine of Diamonds"
+            return "Nine of Diamonds"
         elif self.value == 9 and self.suit == 1:
-            return  "Nine of Spades"
+            return "Nine of Spades"
         elif self.value == 9 and self.suit == 2:
             return "Nine of Hearts"
         elif self.value == 9 and self.suit == 3:
             return "Nine of Clubs"
         if self.value == 10 and self.suit == 0:
-            return  "Ten of Diamonds"
+            return "Ten of Diamonds"
         elif self.value == 10 and self.suit == 1:
-            return  "Ten of Spades"
+            return "Ten of Spades"
         elif self.value == 10 and self.suit == 2:
             return "Ten of Hearts"
         elif self.value == 10 and self.suit == 3:
             return "Ten of Clubs"
         if self.value == 11 and self.suit == 0:
-            return  "Jack of Diamonds"
+            return "Jack of Diamonds"
         elif self.value == 11 and self.suit == 1:
-            return  "Jack of Spades"
+            return "Jack of Spades"
         elif self.value == 11 and self.suit == 2:
             return "Jack of Hearts"
         elif self.value == 11 and self.suit == 3:
             return "Jack of Clubs"
         if self.value == 12 and self.suit == 0:
-            return  "Queen of Diamonds"
+            return "Queen of Diamonds"
         elif self.value == 12 and self.suit == 1:
-            return  "Queen of Spades"
+            return "Queen of Spades"
         elif self.value == 12 and self.suit == 2:
             return "Queen of Hearts"
         elif self.value == 12 and self.suit == 3:
             return "Queen of Clubs"
         if self.value == 13 and self.suit == 0:
-            return  "King of Diamonds"
+            return "King of Diamonds"
         elif self.value == 13 and self.suit == 1:
-            return  "King of Spades"
+            return "King of Spades"
         elif self.value == 13 and self.suit == 2:
             return "King of Hearts"
         elif self.value == 13 and self.suit == 3:
             return "King of Clubs"
         if self.value == 14 and self.suit == 0:
-            return  "Ace of Diamonds"
+            return "Ace of Diamonds"
         elif self.value == 14 and self.suit == 1:
-            return  "Ace of Spades"
+            return "Ace of Spades"
         elif self.value == 14 and self.suit == 2:
             return "Ace of Hearts"
         elif self.value == 14 and self.suit == 3:
             return "Ace of Clubs"
-        if self.value >= 15:
-            raise AssertionError("Value must be between 2 and 14")
